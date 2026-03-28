43e906934d753f9413734fe7d71255811646ecd8
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-03-28 23:53:25 +0530

    contextapi

--- day18-contextapi/src/App.jsx
+++ day18-contextapi/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import DashboardOne from "./components/dashboardOne/DashboardOne";
+import DashboardTwo from "./components/dashboardTwo/DashboardTwo";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [toggle, setToggle] = useState(false);
 
-export default App
+  return (
+    <div>
+      <button onClick={()=> setToggle((prev)=>!prev)}>
+        {toggle ? 'Go To Home' : 'Go To Users'}
+      </button>
+      {toggle ? (
+        <div>
+          <DashboardTwo />
+        </div>
+      ) : (
+        <div>
+          <DashboardOne />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
