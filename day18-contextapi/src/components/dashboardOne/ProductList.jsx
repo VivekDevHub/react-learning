257910592590836d87cb97c03a3f4fcb4470ce48
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-03-28 23:53:36 +0530

    contextapi

--- day18-contextapi/src/components/dashboardOne/ProductList.jsx
+++ day18-contextapi/src/components/dashboardOne/ProductList.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const ProductList = () => {
+const ProductList = ({ product , handleProductDelete }) => {
+  console.log('product dikha =-=-=-=-', product);
   return (
-    <div>ProductList</div>
-  )
-}
+    <div>
+      <h1>{product?.title}</h1>
+      <button onClick={()=>{handleProductDelete(product.id)}}>Delete</button>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
