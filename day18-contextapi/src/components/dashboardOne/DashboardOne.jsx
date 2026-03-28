0c134b843d753d3594b635bf4ae3bb967914f0c6
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-03-28 23:53:30 +0530

    contextapi

--- day18-contextapi/src/components/dashboardOne/DashboardOne.jsx
+++ day18-contextapi/src/components/dashboardOne/DashboardOne.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState } from "react";
+import axios from "axios";
+import ProductList from "./ProductList";
 
 const DashboardOne = () => {
-  return (
-    <div>DashboardOne</div>
-  )
-}
+  const [allProduct, setAllProduct] = useState([]);
 
-export default DashboardOne
+  let fetchAllProduct = async () => {
+    let res = await axios.get("https://fakestoreapi.com/products");
+    setAllProduct(res?.data);
+  };
+  
+  useEffect(() => {
+    fetchAllProduct();
+  }, []);
+
+  let handleProductDelete = (prod_id) => {
+    console.log(prod_id);
+    let filterProducts = allProduct.filter((elem) => elem.id !== prod_id);
+    
+    setAllProduct(filterProducts);
+  };
+
+  return (
+    <div>
+      <h1>Product Home</h1>
+      <div>
+        {allProduct.map((elem) => {
+          return (
+            <ProductList
+              key={elem.id}
+              product={elem}
+              handleProductDelete={handleProductDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardOne;
