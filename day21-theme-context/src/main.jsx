6a3a36722a877ccb3ffa18f36eea4caa4f04d95d
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-03-31 23:57:21 +0530

    theme-context

--- day21-theme-context/src/main.jsx
+++ day21-theme-context/src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
 import {ThemeProvider} from "../context/ThemeContext.jsx"
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
