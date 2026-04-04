ed44cb2d8663489f2e42b45dc01cdde85ea9f3bc
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-04-04 16:17:29 +0530

    protected routes

---
+++ day27-data-routing/src/routes/AuthProtected.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { Navigate, Outlet } from "react-router";
+import { useAuth } from "../context/AuthContext";
+
+const AuthProtected = () => {
+  let { loggedInUser } = useAuth();
+
+  if (loggedInUser) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthProtected;
