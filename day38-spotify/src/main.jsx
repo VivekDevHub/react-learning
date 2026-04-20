023e51f777caa484288e67a532b2150c284f2620
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-04-20 23:45:20 +0530

    project 1 debouncing

--- day38-spotify/src/main.jsx
+++ day38-spotify/src/main.jsx
@@ -1,8 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Provider } from "react-redux";
 import GlobalError from "./shared/components/ GlobalError";
 import { store } from "./app/store/store";
+import { Provider } from "react-redux";
+import AppRoutes from "./app/routes/AppRoutes";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
