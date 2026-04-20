8d34b7cd9ff408f0a2aa51db1f5be17bb038e4e2
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-04-20 23:45:34 +0530

    project 1 debouncing

---
+++ day38-spotify/src/features/player/state/playerSlice.jsx
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let playerSlice = createSlice({
+  name: "player",
+  initialState: {
+    queue: [],
+    currentPlayingSong: null,
+    isPlaying: false,
+  },
+  reducers: {
+    playNewSong: (state, action) => {
+      state.currentPlayingSong = action.payload;
+      state.isPlaying = true;
+    },
+    play: (state) => {
+      state.isPlaying = true;
+    },
+    pause: (state) => {
+      state.isPlaying = false;
+    },
+    nextSong: () => {},
+    prevSong: () => {},
+  },
+});
+
+export let { playNewSong, play, pause } = playerSlice.actions;
+export default playerSlice.reducer;
