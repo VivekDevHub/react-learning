57b8e2b6bb50781d5cb7b29e09875b11681bf2f9
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-04-23 22:51:57 +0530

    spotify-4-layer-architecture

--- day38-spotify/src/main.jsx
+++ day38-spotify/src/main.jsx
@@ -1,13 +1,8 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import GlobalError from "./shared/components/ GlobalError";
-import { store } from "./app/store/store";
-import { Provider } from "react-redux";
-import AppRoutes from "./app/routes/AppRoutes";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <GlobalError />
-    <AppRoutes />
-  </Provider>
-);
+createRoot(document.getElementById('root')).render(
+  <App />
+)
